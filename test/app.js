37d2f0532e4b6052d54000160820053eab227f78
Sebastian Riedel <sri@cpan.org>
2021-04-28 00:35:55 +0200

    Add support for more HTTP methods

--- test/app.js
+++ test/app.js
@@ -7,13 +7,23 @@ t.test('App', async t => {
 
   app.get('/', ctx => ctx.render({text: 'Hello Mojo!'}));
 
-  app.post('/hello', ctx => ctx.render({text: 'Hello World!'}));
+  app.any('/methods', ctx => ctx.render({text: ctx.req.method}));
 
   const client = await test(app, {tap: t});
 
   await t.test('Hello World', async t => {
     (await client.getOk('/')).statusIs(200).headerIs('Content-Length', '11').bodyIs('Hello Mojo!');
-    (await client.postOk('/hello')).statusIs(200).headerIs('Content-Length', '12').bodyIs('Hello World!');
+    t.done();
+  });
+
+  await t.test('Methods', async t => {
+    (await client.deleteOk('/methods')).statusIs(200).bodyIs('DELETE');
+    (await client.getOk('/methods')).statusIs(200).bodyIs('GET');
+    (await client.headOk('/methods')).statusIs(200).bodyIs('');
+    (await client.optionsOk('/methods')).statusIs(200).bodyIs('OPTIONS');
+    (await client.patchOk('/methods')).statusIs(200).bodyIs('PATCH');
+    (await client.postOk('/methods')).statusIs(200).bodyIs('POST');
+    (await client.putOk('/methods')).statusIs(200).bodyIs('PUT');
     t.done();
   });
 
