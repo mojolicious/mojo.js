0d41a839b9878fa68a6d61fe3bf89d275833f211
Sebastian Riedel <sri@cpan.org>
2021-04-28 23:38:43 +0200

    Add support for under

--- test/app.js
+++ test/app.js
@@ -13,6 +13,16 @@ t.test('App', async t => {
     ctx.render({json: data});
   });
 
+  const nested = app.under('/nested', ctx => {
+    const auth = ctx.req.url.searchParams.get('auth');
+    if (auth === '1') return true;
+    ctx.render({text: 'Permission denied'});
+    return false;
+  });
+  nested.get('/', ctx => ctx.render({text: 'Authenticated'}));
+  const test = nested.any('/test').to({prefix: 'X:'});
+  test.any('/methods', ctx => ctx.render({text: ctx.stash.prefix + ctx.req.method}));
+
   const client = await app.newTestClient({tap: t});
 
   await t.test('Hello World', async t => {
@@ -32,6 +42,21 @@ t.test('App', async t => {
 
   await t.test('JSON', async t => {
     (await client.putOk('/json', {json: {hello: 'world'}})).statusIs(200).jsonIs({hello: 'world'});
+  });
+
+  await t.test('Not found', async t => {
+    (await client.putOk('/does_not_exist')).statusIs(404);
+  });
+
+  await t.test('Nested routes', async t => {
+    (await client.getOk('/nested?auth=1')).statusIs(200).bodyIs('Authenticated');
+    (await client.getOk('/nested?auth=0')).statusIs(200).bodyIs('Permission denied');
+    (await client.getOk('/nested/test/methods?auth=1')).statusIs(200).bodyIs('X:GET');
+    (await client.putOk('/nested/test/methods?auth=1')).statusIs(200).bodyIs('X:PUT');
+    (await client.postOk('/nested/test/methods?auth=1')).statusIs(200).bodyIs('X:POST');
+    (await client.getOk('/nested/test/methods?auth=0')).statusIs(200).bodyIs('Permission denied');
+    (await client.getOk('/nested/test?auth=1')).statusIs(404).bodyIs('Not found');
+    (await client.getOk('/nested/test/foo?auth=1')).statusIs(404).bodyIs('Not found');
   });
 
   await client.done();
