ab214f0c287fe86ad739625866b8d24ed46d4b5c
Sebastian Riedel <sri@cpan.org>
2021-05-01 17:39:35 +0200

    Make the request id a little shorter

--- test/app.js
+++ test/app.js
@@ -96,7 +96,7 @@ t.test('App', async t => {
   });
 
   await t.test('Request ID', async t => {
-    (await client.getOk('/request_id')).statusIs(200).bodyLike(/^[0-9]+-[0-9a-f]{8}$/i);
+    (await client.getOk('/request_id')).statusIs(200).bodyLike(/^[0-9]+-[0-9a-z]{6}$/);
   });
 
   await client.done();
