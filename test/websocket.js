65b46443c40e39ad5235d0efc2bebc304a5c513b
Sebastian Riedel <sri@cpan.org>
2021-05-03 01:30:12 +0200

    Make WebSocket roundtrip work

---
+++ test/websocket.js
@@ -0,0 +1,38 @@
+import t from 'tap';
+import mojo from '../index.js';
+
+t.test('App', async t => {
+  const app = mojo();
+
+  app.get('/').to(ctx => ctx.render({text: 'Hello Mojo!'}));
+
+  app.websocket('/ws').to(ctx => {
+    ctx.on('connection', ws => {
+      ws.on('message', message => {
+        ws.send(message);
+      });
+    });
+  });
+
+  const client = await app.newTestClient({tap: t});
+
+  await t.test('Hello World', async t => {
+    (await client.getOk('/')).statusIs(200).bodyIs('Hello Mojo!');
+  });
+
+  await t.test('WebSocket roundtrip', async t => {
+    const ws = client.websocket('/ws');
+    ws.on('open', () => {
+      ws.send('Hello Mojo!');
+    });
+    const message = await new Promise(resolve => {
+      ws.on('message', data => {
+        ws.on('close', () => resolve(data));
+        ws.close();
+      });
+    });
+    t.equal(message, 'Hello Mojo!');
+  });
+
+  await client.done();
+});
