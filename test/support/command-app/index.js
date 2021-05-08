674a6f3d11dcd750be6607c10ffc62cc44db4c39
Roy Storey <kiwiroy@users.noreply.github.com>
2021-05-09 01:11:11 +1200

    add routes command

--- test/support/command-app/index.js
+++ test/support/command-app/index.js
@@ -4,4 +4,8 @@ export const app = mojo();
 
 app.any('/', ctx => ctx.render({text: 'Hello Mojo!'}));
 
+const foo = app.get('/foo');
+foo.post('/bar').name('bar');
+foo.get('/baz', {ext: 'html'});
+
 app.start();
