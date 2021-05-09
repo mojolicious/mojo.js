afeb84c0868c0291813386ffb7c6a2408da1f147
Sebastian Riedel <sri@cpan.org>
2021-05-09 17:04:17 +0200

    More tests for tag helpers

---
+++ test/plugin-app.js
@@ -0,0 +1,36 @@
+import escapeStringRegexp from 'escape-string-regexp';
+import mojo from '../index.js';
+import t from 'tap';
+
+t.test('Plugin app', async t => {
+  const app = mojo();
+
+  app.get('/tag_helpers', ctx => ctx.render({inline: tagHelperPlugin}));
+
+  const client = await app.newTestClient({tap: t});
+
+  await t.test('Tag helpers', async t => {
+    const baseURL = client.server.urls[0] + app.static.prefix.substring(1);
+    (await client.getOk('/tag_helpers')).statusIs(200).bodyIs(tagHelperPluginResult(baseURL));
+  });
+
+  await client.stop();
+});
+
+const tagHelperPlugin = `
+Favicon: <%- helpers.mojoFaviconTag() %>
+Relative script: <%- helpers.scriptTag('/foo/bar.js') %>
+Relative style: <%- helpers.styleTag('/foo/bar.css') %>
+Absolute script: <%- helpers.scriptTag('https://mojojs.org/public/foo/bar.js') %>
+Absolute style: <%- helpers.styleTag('https://mojojs.org/public/foo/bar.css') %>
+`;
+
+function tagHelperPluginResult (baseURL) {
+  return `
+Favicon: <link rel="icon" href="${baseURL}mojo/favicon.ico"></link>
+Relative script: <script src="${baseURL}foo/bar.js"></script>
+Relative style: <link rel="stylesheet" href="${baseURL}foo/bar.css">
+Absolute script: <script src="https://mojojs.org/public/foo/bar.js"></script>
+Absolute style: <link rel="stylesheet" href="https://mojojs.org/public/foo/bar.css">
+`;
+}
