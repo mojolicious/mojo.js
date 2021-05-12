81431526bffc3a676d2c4ff03b8c60096c40f9d2
Sebastian Riedel <sri@cpan.org>
2021-05-12 19:33:11 +0200

    Add second contributor

--- test/command-app.js
+++ test/command-app.js
@@ -64,25 +64,25 @@ t.test('Command app', async t => {
     const output = await captureOutput(async () => {
       await app.cli.start('routes', '-h');
     });
-    t.match(output.toString(), /Usage: APPLICATION routes/, 'correct usage output');
-    t.match(app.cli.commands.routes.description, /Show available routes/, 'correct description');
-    t.match(app.cli.commands.routes.usage, /Usage: APPLICATION routes/, 'correct usage');
+    t.match(output.toString(), /Usage: APPLICATION routes/);
+    t.match(app.cli.commands.routes.description, /Show available routes/);
+    t.match(app.cli.commands.routes.usage, /Usage: APPLICATION routes/);
 
     const output2 = await captureOutput(async () => {
       await app.cli.start('routes');
     });
-    t.match(output2.toString(), /^\/\s+\*/, '/ route correct');
-    t.match(output2.toString(), /^\/foo\s+GET\s+foo/m, '/foo route correct');
-    t.match(output2.toString(), /^\s{2}\+\/bar\s+POST\s+"bar"/m, '/bar route with name');
-    t.match(output2.toString(), /^\s{2}\+\/baz\s+GET\s+baz/m, '/baz route correct');
+    t.match(output2.toString(), /^\/\s+\*/);
+    t.match(output2.toString(), /^\/foo\s+GET\s+foo/m);
+    t.match(output2.toString(), /^\s{2}\+\/bar\s+POST\s+"bar"/m);
+    t.match(output2.toString(), /^\s{2}\+\/baz\s+GET\s+baz/m);
 
     const output3 = await captureOutput(async () => {
       await app.cli.start('routes', '-v');
     });
-    t.match(output3.toString(), /\/\s+\*\s+\/\^\/s/, 'correct pattern');
-    t.match(output3.toString(), /^\/foo\s+GET\s+foo\s+\/\^\\\/foo\/s/m, 'correct pattern');
-    t.match(output3.toString(), /^\s{2}\+\/bar\s+POST\s+"bar"\s+\/\^\\\/bar\/s/m, 'correct pattern');
-    t.match(output3.toString(), /^\s{2}\+\/baz\s+GET\s+baz\s+\/\^\\\/baz\\\/\?\\\.\(html\)\$\/s/m, 'correct pattern');
+    t.match(output3.toString(), /\/\s+\*\s+\/\^\/s/);
+    t.match(output3.toString(), /^\/foo\s+GET\s+foo\s+\/\^\\\/foo\/s/m);
+    t.match(output3.toString(), /^\s{2}\+\/bar\s+POST\s+"bar"\s+\/\^\\\/bar\/s/m);
+    t.match(output3.toString(), /^\s{2}\+\/baz\s+GET\s+baz\s+\/\^\\\/baz\\\/\?\\\.\(html\)\$\/s/m);
   });
 
   await t.test('server', async t => {
