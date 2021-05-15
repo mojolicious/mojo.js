e5f15028294d3a5106995539985a3d46b75a16c6
Sebastian Riedel <sri@cpan.org>
2021-05-15 23:33:58 +0200

    Move test files around a bit

--- test/jsonconfig-app.js
+++ test/jsonconfig-app.js
@@ -1,4 +1,4 @@
-import {app} from './support/app-config/app-config.js';
+import {app} from './support/jsonconfig-app/app.js';
 import File from '../lib/file.js';
 import jsonConfigPlugin from '../lib/plugins/json-config.js';
 import mojo from '../lib/mojo.js';
@@ -7,28 +7,31 @@ import t from 'tap';
 t.test('JSONConfig app', async t => {
   const client = await app.newTestClient({tap: t});
 
-  await t.test('Named config file', async t => {
+  await t.test('External app with config', async t => {
     t.same(app.config, {name: 'Bond. James Bond', drink: 'Martini', car: 'Aston Martin'});
     (await client.getOk('/')).statusIs(200).bodyIs('My name is Bond. James Bond.');
   });
+
   await client.stop();
 
-  t.test('Default JSONConfig App', async t => {
+  t.test('Named config file', t => {
     const app = mojo();
     app.config = {name: 'overridden', extra: 'option'};
-    app.plugin(jsonConfigPlugin, {file: File.currentFile().dirname().child('support', 'named.json').toString()});
+    const file = File.currentFile().dirname().child('support', 'jsonconfig-app', 'custom', 'named.json').toString();
+    app.plugin(jsonConfigPlugin, {file});
     t.same(app.config, {name: 'namedConfig', extra: 'option', deep: {option: ['deep']}});
     t.end();
   });
-  t.test('Test overriding option by changing mode', async t => {
+  t.test('Named config file with custom mode', t => {
     const app = mojo({mode: 'test'});
     app.config = {name: 'overridden', extra: 'option'};
-    app.plugin(jsonConfigPlugin, {file: File.currentFile().dirname().child('support', 'named.json').toString()});
+    const file = File.currentFile().dirname().child('support', 'jsonconfig-app', 'custom', 'named.json').toString();
+    app.plugin(jsonConfigPlugin, {file});
     t.same(app.config, {name: 'namedConfig', extra: 'required', deep: {option: ['deep']}});
     t.end();
   });
 
-  t.test('Missing config file', async t => {
+  t.test('Missing config file', t => {
     const app = mojo();
     t.throws(() => {
       app.plugin(jsonConfigPlugin(app, {file: 'missing.json'}));
