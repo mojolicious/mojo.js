00ad36ea5fcfce91b49ffc2199bc94cb8460a79b
Sebastian Riedel <sri@cpan.org>
2021-05-21 15:07:25 +0200

    Add support for inline layouts

--- test/renderer-app.js
+++ test/renderer-app.js
@@ -20,6 +20,9 @@ t.test('Renderer app', async t => {
 
   app.get('/missing', ctx => ctx.render({view: 'does_not_exist'}));
 
+  app.get('/inline/layout')
+    .to(ctx => ctx.render({inline: inlineTemplate, inlineLayout: inlineLayout}, {what: 'works'}));
+
   const client = await app.newTestClient({tap: t});
 
   await t.test('Custom engine', async t => {
@@ -34,5 +37,17 @@ t.test('Renderer app', async t => {
     (await client.getOk('/missing')).statusIs(500).bodyLike(/Nothing could be rendered/);
   });
 
+  await t.test('Inline layout', async t => {
+    (await client.getOk('/inline/layout')).statusIs(200).bodyLike(/Header.+this works.+Footer/s);
+  });
+
   await client.stop();
 });
+
+const inlineTemplate = 'this <%= what %>';
+
+const inlineLayout = `
+Header
+<%- view.content %>
+Footer
+`;
