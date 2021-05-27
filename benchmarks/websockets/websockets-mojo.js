92a990f217033f00352a6c377d83cc7eeed4c03a
Sebastian Riedel <sri@cpan.org>
2021-05-28 00:43:31 +0200

    Add WebSocket benchmark

---
+++ benchmarks/websockets/websockets-mojo.js
@@ -0,0 +1,22 @@
+import mojo from '../../lib/mojo.js';
+
+const app = mojo();
+
+app.websocket('/ws').to(ctx => {
+  ctx.on('connection', ws => {
+    ws.on('message', message => {
+      ws.send(message);
+    });
+  });
+});
+
+const client = await app.newMockClient();
+
+const ws = await client.websocket('/ws');
+let i = 0;
+ws.on('message', message => {
+  i++ > 100000 ? ws.close() : ws.send(i.toString());
+});
+ws.send('Hello Mojo!');
+
+await client.stop();
