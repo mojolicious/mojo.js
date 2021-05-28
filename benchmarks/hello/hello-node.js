43c1e3810648bc026897844be648ddb978477f53
Sebastian Riedel <sri@cpan.org>
2021-05-28 18:58:48 +0200

    Fix node benchmark

--- benchmarks/hello/hello-node.js
+++ benchmarks/hello/hello-node.js
@@ -1,7 +1,7 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/plain', 'Content-Length': 12});
   res.end('Hello World!');
 });
 server.listen(3000);
