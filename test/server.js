d3da3dbd86e35e43585dea6e904b7f07fd02f9c5
Sebastian Riedel <sri@cpan.org>
2021-06-02 23:00:47 +0200

    Test MOJO_SERVER_DEBUG

--- test/server.js
+++ test/server.js
@@ -2,6 +2,10 @@ import mojo from '../lib/mojo.js';
 import t from 'tap';
 
 t.test('Server', async t => {
+  const app = mojo();
+
+  app.get('/', ctx => ctx.render({text: 'Hello World!'}));
+
   t.test('listenArgsforURL', t => {
     t.same(mojo.Server.listenArgsForURL(new URL('http://*')), [null, '0.0.0.0']);
     t.same(mojo.Server.listenArgsForURL(new URL('http://*:3000')), [3000, '0.0.0.0']);
@@ -13,5 +17,25 @@ t.test('Server', async t => {
     t.same(mojo.Server.listenArgsForURL(new URL('http://[::1]:5000')), [5000, '::1']);
 
     t.end();
+  });
+
+  await t.test('MOJO_SERVER_DEBUG', async t => {
+    process.env.MOJO_SERVER_DEBUG = 1;
+    const client = await app.newTestClient({tap: t});
+
+    let res;
+    const output = await mojo.util.captureOutput({stderr: true, stdout: false}, async () => {
+      res = await client.get('/');
+    });
+    t.equal(res.status, 200);
+    t.equal(await res.text(), 'Hello World!');
+    t.match(output, /Server <<< Client/);
+    t.match(output, /GET \//);
+    t.match(output, /Host: /);
+    t.match(output, /Server >>> Client/);
+    t.match(output, /Content-Length: /);
+    t.match(output, /Hello World!/);
+
+    await client.stop();
   });
 });
