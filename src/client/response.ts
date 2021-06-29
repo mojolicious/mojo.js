40fde13a4632a7208d10d3374d5cd51fcd86bfd8
Marcus Ramberg <marcus@means.no>
2021-06-29 11:20:20 +0200

    Port client response to typescript

--- src/client/response.ts
+++ src/client/response.ts
@@ -1,39 +1,39 @@
 import Body from '../body.js';
 
 export default class ClientResponse extends Body {
-  get isClientError () {
+  get isClientError (): boolean {
     const statusCode = this.status;
     return statusCode >= 400 && statusCode <= 499;
   }
 
-  get isError () {
+  get isError (): boolean {
     return this.isClientError || this.isServerError;
   }
 
-  get isRedirect () {
+  get isRedirect (): boolean {
     const statusCode = this.status;
     return statusCode >= 300 && statusCode <= 399;
   }
 
-  get isServerError () {
+  get isServerError (): boolean {
     const statusCode = this.status;
     return statusCode >= 500 && statusCode <= 599;
   }
 
-  get isSuccess () {
+  get isSuccess (): boolean {
     const statusCode = this.status;
     return statusCode >= 200 && statusCode <= 299;
   }
 
-  get status () {
-    return this.raw.statusCode;
+  get status (): number {
+    return this.raw.statusCode as number;
   }
 
-  get statusMessage () {
+  get statusMessage (): string | undefined {
     return this.raw.statusMessage;
   }
 
-  get type () {
+  get type (): string | undefined {
     return this.headers['content-type'];
   }
 }
