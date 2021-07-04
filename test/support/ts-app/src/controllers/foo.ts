2e166a80156b69d561f886774dde2e3df86c1ec7
Sebastian Riedel <sri@cpan.org>
2021-07-04 15:54:01 +0200

    Encourage proper class names for controllers

--- test/support/ts-app/src/controllers/foo.ts
+++ test/support/ts-app/src/controllers/foo.ts
@@ -1,6 +1,6 @@
 import type {MojoContext} from '../../../../../lib/core.js';
 
-export default class Controller {
+export default class FooController {
   async hello (ctx: MojoContext): Promise<void> {
     await ctx.render({text: 'Hello TypeScript controller!'});
   }
