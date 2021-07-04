a6594918cd36b5a5d28b1225ce3343990efdc854
Sebastian Riedel <sri@cpan.org>
2021-07-04 18:22:23 +0200

    Also include a model in the TypeScript test

--- test/support/ts-app/src/index.ts
+++ test/support/ts-app/src/index.ts
@@ -1,14 +1,18 @@
 import mojo from '../../../../lib/core.js';
+import {Bar} from './models/bar.js';
 import helpersPlugin from './plugins/helpers.js';
 
 export const app = mojo();
 
 app.log.level = 'info';
 
+app.models.bar = new Bar();
+
 app.plugin(helpersPlugin);
 
 app.get('/', async ctx => {
-  await ctx.render({text: 'Hello TypeScript!'});
+  const language: string = ctx.models.bar.language();
+  await ctx.render({text: `Hello ${language}!`});
 });
 
 app.get('/hello').to('foo#hello');
--- test/support/ts-app/src/plugins/helpers.ts
+++ test/support/ts-app/src/plugins/helpers.ts
@@ -1,7 +1,10 @@
 import type {MojoApp, MojoContext} from '../../../../../lib/types.js';
 
 export default function helpersPlugin (app: MojoApp): void {
-  app.addHelper('testHelper', async (ctx: MojoContext, partial: string): Promise<string> => {
-    return `TypeScript ${partial}`;
-  });
+  app.addHelper('testHelper', testHelper);
+}
+
+async function testHelper (ctx: MojoContext, partial: string): Promise<string> {
+  const language: string = ctx.models.bar.language();
+  return `${language} ${partial}`;
 }
