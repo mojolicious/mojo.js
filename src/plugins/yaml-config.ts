df114b3d32ce6829656926f05d513ff2a13ef593
Sebastian Riedel <sri@cpan.org>
2021-10-25 18:03:14 +0200

    Fix plugin name

--- src/plugins/yaml-config.ts
+++ src/plugins/yaml-config.ts
@@ -2,7 +2,7 @@ import type {MojoApp, ConfigOptions} from '../types.js';
 import {loadConfig} from './json-config.js';
 import yaml from 'js-yaml';
 
-export default function jsonConfigPlugin(app: MojoApp, options: ConfigOptions): void {
+export default function yamlConfigPlugin(app: MojoApp, options: ConfigOptions): void {
   if (options.ext === undefined) options.ext = 'yml';
   loadConfig(app, options, yaml.load);
 }
