f7d29d68dbf497724a6921ebc9e1576a4739eae4
Sebastian Riedel <s@kraih.com>
2022-05-27 14:36:21 +0200

    Use the same test cases as Mojolicious in the example app

--- examples/responses.js
+++ examples/responses.js
@@ -11,31 +11,35 @@ app.get('/res1', ctx => {
 });
 
 app.get('/res2', async ctx => {
+  await ctx.res.length(11).send(Stream.Readable.from(['Hello', 'World!']));
+});
+
+app.get('/res3', async ctx => {
   ctx.res.set('Transfer-Encoding', 'chunked');
   await ctx.res.send(Stream.Readable.from(['Hello', 'World!']));
 });
 
-app.get('/res3', ctx => {
+app.get('/res4', ctx => {
   return ctx.render({text: '', status: 204});
 });
 
-app.get('/res4', () => {
+app.get('/res5', () => {
   throw new Error('Hello World!');
 });
 
-app.get('/res5', ctx => {
+app.get('/res6', ctx => {
   return ctx.render({inline: "<% throw new Error('Hello World!') %>"});
 });
 
-app.get('/res6', ctx => {
+app.get('/res7', ctx => {
   return ctx.res.status(200).send('Hello World!');
 });
 
-app.get('/res7', ctx => {
+app.get('/res8', ctx => {
   return ctx.render({json: {hello: 'world'}});
 });
 
-app.get('/res8', async () => {
+app.get('/res9', async () => {
   function myAsyncFunction() {
     return new Promise(() => {
       throw 'Just a string!';
