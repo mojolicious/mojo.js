8810e8b2160b410a368b3b79920191a11223baf9
Sebastian Riedel <s@kraih.com>
2022-11-19 22:45:07 +0100

    Use the latest version of @mojojs/util

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,20 +1,11 @@
 import type {MojoApp} from './types.js';
-type Hook = (...args: any[]) => any;
+import {AsyncHooks} from '@mojojs/util';
 
 /**
  * Hook class.
  */
-export class Hooks {
-  _hooks: Record<string, Hook[]> = {};
+export class Hooks extends AsyncHooks {
   _lifecycleHookScore = 0;
-
-  /**
-   * Add hook.
-   */
-  addHook(name: string, fn: Hook): void {
-    if (this._hooks[name] === undefined) this._hooks[name] = [];
-    this._hooks[name].push(fn);
-  }
 
   /**
    * Run `command:start` hook with dependencies. Note that this method is EXPERIMENTAL and might change without
@@ -33,14 +24,6 @@ export class Hooks {
   async commandAfter(app: MojoApp, commandArgs: string[]): Promise<void> {
     await this.runHook('command:after', app, commandArgs);
     await this._appStop(app);
-  }
-
-  /**
-   * Run hook.
-   */
-  async runHook(name: string, ...args: any[]): Promise<any> {
-    if (this._hooks[name] === undefined) return await Promise.resolve();
-    return await this._prepareHook(this._hooks[name])(...args);
   }
 
   /**
@@ -67,17 +50,5 @@ export class Hooks {
   async _appStop(app: MojoApp): Promise<void> {
     this._lifecycleHookScore--;
     if (this._lifecycleHookScore === 0) await this.runHook('app:stop', app);
-  }
-
-  _prepareHook(chain: Hook[]) {
-    return async function hook(...args: any[]): Promise<any> {
-      return await next(0);
-
-      async function next(i: number, result?: any): Promise<any> {
-        const fn = chain[i];
-        if (result !== undefined || fn === undefined) return result;
-        return await new Promise(resolve => resolve(fn(...args))).then(next.bind(null, i + 1));
-      }
-    };
   }
 }
